f56c9ab3e7b9b11e9a9f1be0eca27d61dce3a0ea
david-hankinson <davidmhankinson@outlook.com>
2026-01-04 17:35:40 -0700

    0.0.1

--- test/vpc_test.go
+++ test/vpc_test.go
@@ -4,29 +4,16 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/require"
 )
 
-func TestNetworkBasics(t *testing.T) {
+func TestPlanSucceeds(t *testing.T) {
 	t.Parallel()
 
 	opts := &terraform.Options{
 		TerraformDir:    "../module",
-		TerraformBinary: "tofu", // 👈 OpenTofu
+		TerraformBinary: "tofu", // 👈 OpenTofu CLI
 	}
 
-	defer terraform.Destroy(t, opts)
-	terraform.InitAndApply(t, opts)
-
-	// 1) VPC CIDR is correct
-	vpcCidr := terraform.Output(t, opts, "vpc_cidr")
-	require.Equal(t, "10.1.0.0/16", vpcCidr)
-
-	// 2) Created 2 public subnets
-	publicSubnetIDs := terraform.OutputList(t, opts, "public_subnet_ids")
-	require.Len(t, publicSubnetIDs, 2)
-
-	// 3) Private subnets have correct CIDRs
-	privateCidrs := terraform.OutputList(t, opts, "private_subnet_cidrs")
-	require.ElementsMatch(t, []string{"10.1.3.0/24", "10.1.4.0/24"}, privateCidrs)
+	terraform.Init(t, opts)
+	terraform.Plan(t, opts) // ✅ test fails if plan errors
 }
